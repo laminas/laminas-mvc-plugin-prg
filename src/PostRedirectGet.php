7cc4e3febd83c1c3bbbf0cd05541d6b794d8912b
Andi Rückauer <arueckauer@gmail.com>
2021-09-30 10:01:01 +0200

    Remove file headers
    Signed-off-by: Andi Rückauer <arueckauer@gmail.com>

--- src/PostRedirectGet.php
+++ src/PostRedirectGet.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas/laminas-mvc-plugin-prg for the canonical source repository
- * @copyright https://github.com/laminas/laminas-mvc-plugin-prg/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-mvc-plugin-prg/blob/master/LICENSE.md New BSD License
- */
-
 namespace Laminas\Mvc\Plugin\Prg;
 
 use Laminas\Mvc\Controller\Plugin\AbstractPlugin;
